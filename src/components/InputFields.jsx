279d4abfc5fbc5c9059bf1991dd4c8d78d9e6450
Alex_Bellamy <alexlbellamy@gmail.com>
2020-07-05 12:30:30 +0200

    adding input fields for distance, age, login and password

--- src/components/InputFields.jsx
+++ src/components/InputFields.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { Input } from 'semantic-ui-react'
 
 const InputFields = ({ onChangeHandler }) => {
   return (
     <>
-      <label>Distance</label>
-      <input onChange={onChangeHandler} name="distance" id="distance"></input>
+    <Input>
+      <input onChange={onChangeHandler} name="distance" id="distance" placeholder="Distance in metres"></input>
+    </Input>
 
       <select onChange={onChangeHandler} name="gender" id="gender">
         <option value="female">Female</option>
         <option value="male">Male</option>
       </select>
 
-      <label>Age</label>
-      <input onChange={onChangeHandler} name="age" id="age"></input>
+    <Input>
+      <input onChange={onChangeHandler} name="age" id="age" placeholder="Age"></input>
+    </Input>
     </>
   );
 };
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Input } from 'semantic-ui-react'
 
 const LoginForm = ({ submitFormHandler }) => {
     return (
         <form onSubmit ={submitFormHandler} id="login-form">
+            <Input>
             <label>Email</label>
             <input name="email" type="email" id="email"></input>
+            </Input>
 
+            <Input>
             <label>Password</label>
             <input name="password" type="password" id="password"></input>
+            </Input>
 
             <button id="submit">Submit</button>
         </form>
